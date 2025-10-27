c90c3ce0ad3b84a5b1b8174f5a1d84226592b9c9
Louis <louis.filhol-valantin@epitech.eu>
2025-10-27 17:29:42 +0100

    fix: modified CORS to not only accept CORS but also the FRONTEND_URL from .env

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,7 +19,13 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000', 'http://localhost:5173', 'http://127.0.0.1:3000', 'http://127.0.0.1:5173'],
+    'allowed_origins' => array_filter([
+        'http://localhost:3000',
+        'http://localhost:5173',
+        'http://127.0.0.1:3000',
+        'http://127.0.0.1:5173',
+        env('FRONTEND_URL'),
+    ]),
 
     'allowed_origins_patterns' => [],
 
